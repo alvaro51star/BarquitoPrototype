ecc11ccbc1111aa86935ff07cff1fc17a3115d0a
alvaro51star <alvaro51star@hotmail.es>
2024-03-03 03:44:56 +0100

    Avances

--- BarquitoPrototype/Assets/_Scripts/Boat/Floater.cs
+++ BarquitoPrototype/Assets/_Scripts/Boat/Floater.cs
@@ -8,12 +8,22 @@ public class Floater : MonoBehaviour
     [SerializeField] private float depthBeforeSubmerge = 1f;
     [SerializeField] private float displacementAmount = 3f;
 
+    [SerializeField] private int floaterCount = 1;
+    [SerializeField] private float waterDrag = 0.99f;
+    [SerializeField] private float waterAngularDrag = 0.5f;
+
     private void FixedUpdate()
     {
-        if (transform.position.y < 0f)
+        rb.AddForceAtPosition(Physics.gravity / floaterCount, transform.position, ForceMode.Acceleration);
+
+        float waveHeigth = WaveManager.instance.GetWaveHeight(transform.position.x);
+
+        if (transform.position.y < waveHeigth)
         {
-            float displacementMultiplier = Mathf.Clamp01(-transform.position.y / depthBeforeSubmerge) * displacementAmount;
-            rb.AddForce(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), ForceMode.Acceleration);    
+            float displacementMultiplier = Mathf.Clamp01((waveHeigth - transform.position.y) / depthBeforeSubmerge) * displacementAmount;
+            rb.AddForceAtPosition(new Vector3(0f, Mathf.Abs(Physics.gravity.y) * displacementMultiplier, 0f), transform.position, ForceMode.Acceleration);
+            rb.AddForce(displacementMultiplier * -rb.velocity * waterDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            rb.AddTorque(displacementMultiplier * -rb.angularVelocity * waterAngularDrag * Time.fixedDeltaTime, ForceMode.VelocityChange);
         }
     }
 }
