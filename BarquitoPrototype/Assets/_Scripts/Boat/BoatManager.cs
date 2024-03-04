9d52e7a94dcb68d8d31fb570a78f04e83ca2f837
mirikiwi <CoolKukinator@gmail.com>
2024-03-04 20:28:29 +0100

    Cambios en desgaste del barco

--- BarquitoPrototype/Assets/_Scripts/Boat/BoatManager.cs
+++ BarquitoPrototype/Assets/_Scripts/Boat/BoatManager.cs
@@ -5,18 +5,60 @@ using UnityEngine;
 public class BoatManager : MonoBehaviour
 {
     //Variables
+    [Header("Desgaste")]
     [SerializeField] private float m_currentBienestarBarco;
     [SerializeField] private float m_maxBienestarBarco;
     [SerializeField] private float m_desgasteMultiplier;
+    [SerializeField] private int m_secondsToFirstHit;
+    private bool m_primerGolpe = true;
+    private IEnumerator m_primerGolpeCorutina;
+
+    [Header("Tags")]
+    [SerializeField] private string m_repostarTag = "";
 
     private void Start()
     {
-        m_currentBienestarBarco = m_maxBienestarBarco;
+        ArreglarBarco();
+        m_primerGolpeCorutina = PrimerGolpeCorutina();
     }
 
     public void Desgaste(float iceMassPorcentage)
     {
-        m_currentBienestarBarco -= iceMassPorcentage * m_desgasteMultiplier;
+        if (m_primerGolpe)
+        {
+            m_currentBienestarBarco -= iceMassPorcentage * m_desgasteMultiplier;
+            m_primerGolpe = false;
+            Debug.Log("hacesesto?");
+        }
+        else
+        {
+            m_currentBienestarBarco -= iceMassPorcentage * (m_desgasteMultiplier / 2);
+            StopCoroutine(m_primerGolpeCorutina);
+        }
+        StartCoroutine(m_primerGolpeCorutina);
         UIManager.instance.DesgasteFillImage(m_currentBienestarBarco, m_maxBienestarBarco);
+    }
+
+    private IEnumerator PrimerGolpeCorutina()
+    {
+        yield return new WaitForSeconds(m_secondsToFirstHit);
+        Debug.Log("primer golpe true");
+        m_primerGolpe = true;
+
+    }
+
+    public void ArreglarBarco()
+    {
+        m_currentBienestarBarco = m_maxBienestarBarco;
+        UIManager.instance.DesgasteFillImage(m_currentBienestarBarco, m_maxBienestarBarco);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        string tag = other.tag;
+        if (tag == m_repostarTag)
+        {
+            ArreglarBarco();
+        }
     }
 }
