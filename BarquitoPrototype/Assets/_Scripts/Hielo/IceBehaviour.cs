2f6f81c02e75c59c4fdf1ad4d3998e0a418d787c
mirikiwi <CoolKukinator@gmail.com>
2024-03-05 09:36:04 +0100

    Pequeña prueba con el hielo

--- BarquitoPrototype/Assets/_Scripts/Hielo/IceBehaviour.cs
+++ BarquitoPrototype/Assets/_Scripts/Hielo/IceBehaviour.cs
@@ -8,16 +8,19 @@ public class IceBehaviour : MonoBehaviour
     //Variables
     [SerializeField] private GameObject m_intactIce, m_brokenIce;
     private Animation m_iceAnimation;
+    private Collider m_collider;
 
     private void Start()
     {
         m_iceAnimation = GetComponent<Animation>();
+        m_collider = GetComponent<Collider>();
     }
 
     public void BreakIce()
     {
         m_intactIce.SetActive(false);
         m_brokenIce.SetActive(false);
+        //m_collider.enabled = false;
         //m_iceAnimation.Play();
         //gameObject.SetActive(false);
     }
